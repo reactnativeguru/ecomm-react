83ce26f87a419b84aae272d4be2fbc01a48247b4
John <john.amuesi@wipro.com>
2018-12-30 13:57:40 +0000

    added Nextjs and enabled routing with push state

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ecommerce React App
+
+## Packages
+
+1. NextJs: https://nextjs.org/l
+
+
+
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link'
+
+const Home = props => (
+    <div>
+        <p>
+            Hi
+        </p>
+        <Link href="/sell">
+            <a>Sell</a>
+        </Link>
+    </div>
+
+)
+export default Home
